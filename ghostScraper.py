e9026ade42452da5921d2aaeaec0b665a69f3e64
Alan Braeley <tristan.braeley25@gmail.com>
2020-12-02 23:30:20 -0500

    Putting together the skeleton of the webcrawling+scraping code.

--- ghostScraper.py
+++ ghostScraper.py
@@ -1,1 +1,26 @@
 # ghostScraper script
+import scrapy
+
+
+# Build web crawling spider
+class spider(scrapy.Spider):
+    name = "pages"
+
+
+    def parser(self,response):
+
+
+
+
+
+
+# Spider scrapes the pages
+
+
+
+
+
+
+
+
+# Spider feeds the pages to the natural language processor
