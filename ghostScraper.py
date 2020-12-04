306ca4f7884735ae91b55422a5fcd74b91b52361
Alan Braeley <tristan.braeley25@gmail.com>
2020-12-03 23:58:46 -0500

    Added some of the journals that will be searched

--- ghostScraper.py
+++ ghostScraper.py
@@ -6,6 +6,13 @@ import scrapy
 class spider(scrapy.Spider):
     name = "pages"
 
+    urls = [
+    'https://www.journals.elsevier.com/planetary-and-space-science/recent-articles',
+    'https://pubs.geoscienceworld.org/geology/issue/current',
+    'https://pubs.geoscienceworld.org/geophysics/issue/current',
+    'https://www.journals.elsevier.com/icarus/recent-articles'
+
+    ]
 
     def parser(self,response):
 
